efaf7eed167546f2618c10d0c33d544e723d7323
1097nikki <150649812+1097nikki@users.noreply.github.com>
2026-09-10 03:37:54 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/brand-cases/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
